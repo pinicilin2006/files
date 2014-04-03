05b5981664c1e149639caec14d870379e0fd07d4
pinicilin2006 <pinicilin2006@ya.ru>
2014-04-03 17:36:01 +0600

    Update clear_files.php
    Убрал комментарии

--- crontab/clear_files.php
+++ crontab/clear_files.php
@@ -5,12 +5,6 @@ connect_to_base();
 $date_now=date("Y-m-d");
 $query=mysql_query("SELECT * FROM main");
 while($row = mysql_fetch_array($query)){
-	// echo "Дата добавление ".$row["date_insert"];
-	// echo "<br>";
-	// echo "Срок хранения".$row["srok"];
-	// echo "<br>";
-	// echo (strtotime($date_now) - strtotime($row["date_insert"])) / 86400;
-	// echo "<br>";
 $srok_now = (strtotime($date_now) - strtotime($row["date_insert"])) / 86400;
 	if($srok_now > $row["srok"]){
 		if(file_exists("../files/$row[new_name]")){
